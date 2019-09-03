9901d48814f6f845b23edde586f3914e0d43dff2
Peter Battaglia <petersbattaglia@outlook.com>
2019-09-02 22:00:57 -0700

    icons, layout, text

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <div style="float: left;">
-      <img src="../img/profile_pic.jpg" />
-    </div>
-    <div style="float: right;">
-      Hello
-    </div>
-
+    <Main msg="Welcome to Peter's Website"/>
     <!--
     linkedin
     github
     resume blurbs
     work history
     contact information
-    <Main msg="Welcome to Peter's Website"/>
+
     -->
   </div>
 </template>
@@ -30,12 +24,16 @@ export default {
 </script>
 
 <style>
+body, html {
+  height: 100%;
+  margin: 0px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin: -10px;
-  height: 100vh;
+  height: 100%;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
-    <div style="float: left;">
-      <img src="../../img/profile_pic.jpg" />
-    </div>
-    <div style="float: right;">
-      Hello
+    <img class="photo photo-container" src="../../img/profile_pic.jpg" />
+    <div class="right-panel">
+      <table>
+        <tr>
+          <td>
+	    <div class="bio">
+	      <h1>Peter Battaglia</h1>
+              <h2>Software Engineering Leader</h2>
+	      <h3>Seattle, Washington</h3>
+              <p>I am a deeply technical engineering leader with a passion for motivating teams to deliver outsized results towards product goals. I have a demonstrated track record of immersing myself into uncharted technical territories; creating product areas that lead to customer happiness and revenue growth.</p>
+            </div> 
+          </td>
+          <td>
+	    <div style="margin-top: 100px;">
+              <a href="https://www.linkedin.com/in/petersbattaglia/" target="_blank">
+                <img class="contact-icon" src="../../img/linked_in.png" />
+              </a>
+              <br /><br />
+              <a href="https://github.com/petersbattaglia" target="_blank">
+                <img class="contact-icon" src="../../img/github.svg" />
+              </a>
+	      <br /><br />
+              <a href="https://facebook.com/petersbattaglia" target="_blank">
+                <img class="contact-icon" src="../../img/facebook.jpeg" />
+              </a>
+	      <br /><br />
+              <a href="mailto:petersbattaglia@outlook.com" target="_blank">
+                <img class="contact-icon" src="../../img/mail.png" />
+              </a>
+            </div>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -12,18 +40,49 @@
 <script>
 export default {
   name: 'Main',
-  props: {
-    msg: String
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.img {
-    position: absolute;
-    height: 100%;
-    top: 0;
-    bottom: 0;
+.photo-container {
+    max-height: 100vh;
+    width: 45vw;
+    float: left;
 }
+
+h1 {
+    margin: 0;
+}
+
+h2 {
+    margin: 0;
+}
+
+h3 {
+    margin: 0;
+}
+
+.bio {
+    text-align: left;
+    margin-right: 20px;
+}
+
+.right-panel {
+    float: right;
+    width: 50vw;
+    min-width: 25%;
+    text-align: right;
+    margin-top: 20px;
+    margin-right: 30px;
+}
+
+.contact-icon {
+    height: 64px;
+    width: 64px;
+}
+
+.photo {
+    opacity: 0.5;
+}
+
 </style>
