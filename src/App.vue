13715e45bdf37dcedf6ae4023d222382999082d8
Peter Battaglia <petey@Peters-MacBook-Pro.local>
2019-09-02 19:36:17 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div style="float: left;">
+      <img src="../img/profile_pic.jpg" />
+    </div>
+    <div style="float: right;">
+      Hello
+    </div>
+
+    <!--
+    linkedin
+    github
+    resume blurbs
+    work history
+    contact information
+    <Main msg="Welcome to Peter's Website"/>
+    -->
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: -10px;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div style="float: left;">
+      <img src="../../img/profile_pic.jpg" />
+    </div>
+    <div style="float: right;">
+      Hello
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.img {
+    position: absolute;
+    height: 100%;
+    top: 0;
+    bottom: 0;
+}
+</style>
